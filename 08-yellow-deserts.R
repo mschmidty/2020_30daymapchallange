14686ff3e4cffcd4bdb19ef3a113cf8808c93be7
Mike Schmidt <mschmidty@gmail.com>
2020-11-11 07:50:48 -0700

    started desert map

--- 08-yellow-deserts.R
+++ 08-yellow-deserts.R
@@ -13,3 +13,15 @@ veg2<-raster("data/large_files/US_140EVT_2018/Grid/us_140evt.ovr")
 
 
 poly<-st_read("data/large_files/US_140EVT_2018/Spatial_Metadata/US_140_SPATPLYGN.shp")
+
+meta<-read_csv("data/large_files/US_140EVT_2018/CSV_Data/LF_140EVT_09152016.csv")
+
+meta
+
+
+
+
+
+plot(veg2)
+
+
